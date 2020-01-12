ec36e1fe22222a8a0c1529343e62a7ae45652285
Dan Lipert <danlipert@gmail.com>
2020-01-12 21:03:56 +0900

    working from paste

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@
 use vst2::buffer::AudioBuffer;
 use vst2::plugin::{Plugin, Info};
 
-
-
 struct DigiDist {
     threshold: f32
 }
@@ -12,7 +10,7 @@ struct DigiDist {
 impl Default for DigiDist {
     fn default() -> DigiDist {
         DigiDist {
-            threshold: 1.0
+            threshold: 1.0 // VST parameters are always 0.0 to 1.0
         }
     }
 }
@@ -21,8 +19,8 @@ impl Plugin for DigiDist {
     fn get_info(&self) -> Info {
         Info {
             name: "DigiDist".to_string(),
-            vendor: "Dan Lipert".to_string(),
-            unique_id: 3425221,
+            vendor: "17cupsofcoffee".to_string(),
+            unique_id: 25032017,
 
             inputs: 2,
             outputs: 2,
@@ -41,6 +39,7 @@ impl Plugin for DigiDist {
 
     fn set_parameter(&mut self, index: i32, value: f32) {
         match index {
+            // We don't want to divide by zero, so we'll clamp the value
             0 => self.threshold = value.max(0.01),
             _ => (),
         }
@@ -53,36 +52,39 @@ impl Plugin for DigiDist {
         }
     }
 
-    fn get_parameter_text(&self, index:i32) -> String {
+    fn get_parameter_text(&self, index: i32) -> String {
         match index {
+            // Convert to a percentage
             0 => format!("{}", self.threshold * 100.0),
             _ => "".to_string(),
         }
     }
 
     fn get_parameter_label(&self, index: i32) -> String {
-        match index{
+        match index {
             0 => "%".to_string(),
             _ => "".to_string(),
         }
     }
 
     fn process(&mut self, buffer: AudioBuffer<f32>) {
+        // Split out the input and output buffers into two vectors
         let (inputs, outputs) = buffer.split();
 
+        // For each buffer, transform the samples
         for (input_buffer, output_buffer) in inputs.iter().zip(outputs) {
             for (input_sample, output_sample) in input_buffer.iter().zip(output_buffer) {
+
                 if *input_sample >= 0.0 {
                     *output_sample = input_sample.min(self.threshold) / self.threshold;
-                } else {
+                }
+                else {
                     *output_sample = input_sample.max(-self.threshold) / self.threshold;
                 }
+
             }
         }
     }
-
 }
 
-
 plugin_main!(DigiDist);
-
